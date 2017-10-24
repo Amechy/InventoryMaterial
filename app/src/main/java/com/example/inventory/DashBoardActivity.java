301ca246127b892f5ffb8439e0d0943e338a7d7d
Alejandro Mechine Gamarra <alejandromechinegamarra@gmail.com>
2017-10-24 12:47:42 +0200

    Modificando interfaz producto

--- app/src/main/java/com/example/inventory/DashBoardActivity.java
+++ app/src/main/java/com/example/inventory/DashBoardActivity.java
@@ -1,22 +1,20 @@
 package com.example.inventory;
 
 import android.content.Intent;
-import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayout;
 import android.view.View;
 import android.widget.ImageView;
 
-import java.util.ArrayList;
-import java.util.Vector;
-
-import static android.support.annotation.Dimension.DP;
-
 public class DashBoardActivity extends AppCompatActivity {
-
+    private static int INVENTORY;
+    private static int PRODUCT;
     private GridLayout gridDashBoard;
     private ClickListenerDashboard listenerDashboard;
+    int[] images= {
+            R.drawable.product, R.drawable.inventory,R.drawable.secciones, R.drawable.dependencias, R.drawable.preferencias
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +26,11 @@ public class DashBoardActivity extends AppCompatActivity {
 
         //Definimos un array de int que contenemos las imagenes inventory, product, dependencias,secciones,preferencias.
 
-        int[] images= {
-                R.drawable.producto, R.drawable.intentory,R.drawable.secciones, R.drawable.dependencias, R.drawable.preferencias
-        };
-        int[] idImages= {
+
+        /*int[] idImages= {
                 R.id.imvProducto, R.id.imvInventory, R.id.imvSecciones, R.id.imvDependencias, R.id.imvPreferencias
         };
+        */
 
 
         // Definir un array de ImageView o en este caso un Vector
@@ -70,7 +67,7 @@ public class DashBoardActivity extends AppCompatActivity {
         {
             imageView = new ImageView(this);
             imageView.setImageResource(images[i]);
-            imageView.setId(idImages[i]);
+            GenerateId(i,imageView);
             GridLayout.LayoutParams params = new GridLayout.LayoutParams();
             params.width=width;
             params.height=height;
@@ -85,22 +82,40 @@ public class DashBoardActivity extends AppCompatActivity {
             gridDashBoard.addView(imageView);
         }
 
+
+
     }
+    private void GenerateId(int i, ImageView iv) {
+        switch(images[i]){
+            case R.drawable.inventory:
+                INVENTORY = View.generateViewId();
+                iv.setId(INVENTORY);
+                break;
+
+            case R.drawable.product:
+                PRODUCT = View.generateViewId();
+                iv.setId(PRODUCT);
+                break;
+
+        }
+    }
+
+
+
    class ClickListenerDashboard implements View.OnClickListener{
 
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent();
-            switch (v.getId())
-            {
-                case R.id.imvInventory:
-                    intent = new Intent(DashBoardActivity.this, InventoryActivity.class);
-                    break;
-                case R.id.imvProducto:
-                    intent = new Intent(DashBoardActivity.this, ProductActivity.class);
-                    break;
+            Intent intent;
+
+            if (v.getId()==INVENTORY) {
+                intent = new Intent(DashBoardActivity.this, InventoryActivity.class);
+                startActivity(intent);
             }
-            startActivity(intent);
+            if (v.getId()==PRODUCT) {
+                intent = new Intent(DashBoardActivity.this, ProductActivity.class);
+                startActivity(intent);
+            }
         }
     }
 
